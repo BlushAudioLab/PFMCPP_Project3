d8a4e7a231d8888671fc7fc3e0498f2a18755833
Blush Audio <blushaudio@gmail.com>
2020-05-25 16:59:49 +0000

    Finished Project 3 Assignment.

--- main.cpp
+++ main.cpp
@@ -35,51 +35,51 @@ Sub Object 1: Steering Wheel
     Name 4 things you'll find on the:    Steering Wheel
         1) paddle shifters
         2) 'cruise control' controls
-        3) 
-        4) 
+        3) stereo controls
+        4) horn
     Name 2 things you can do with the:   Steering Wheel
         1) adjust cruise control settings.
-        2)
+        2) turn the car
         
 Sub Object 2: Instrument Cluster
     Name 4 things you'll find on the:   Instrument Cluster
-        1)
-        2)
-        3)
-        4)
+        1) speedometer
+        2) tachometer
+        3) rev meter
+        4) fuel level
     Name 3 things you can do with the:   Instrument Cluster
-        1)
-        2)
-        3)
+        1) check revs
+        2) check speed
+        3) check fuel level
     
 Sub Object 3: Environment Controls
     Name 3 things you'll find on the:    Environment Controls
-        1)
-        2)
-        3)
+        1) temperature
+        2) fan speed
+        3) front rear balance
     Name 3 things you can do with the:   Environment Controls
-        1)
-        2)
-        3)
+        1) adjust temperature
+        2) adjust the fan speed
+        3) adjust the balance between front and rear tempeatures
 
 Sub Object 4: Infotainment System
     Name 3 things you'll find on the:    Infotainment System
-        1)
-        2)
-        3)
+        1) volume control
+        2) playlist
+        3) on/off
     Name 3 things you can do with the:   Infotainment System
-        1)
-        2)
-        3)
+        1) turn it on or off
+        2) change the song
+        3) adjust the volume
 
 Sub Object 5: Seat 
     Name 3 things you'll find on the:    Seat
-        1)
-        2)
-        3)
+        1) seat belt
+        2) leather
+        3) headrest
     Name 2 things you can do with the:   Seat
-        1)
-        2)
+        1) adjust the position
+        2) adjst the angle
 */
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
@@ -96,7 +96,117 @@ Sub Object 5: Seat
  */
 
 #include <iostream>
+
+struct House // UDT 1
+{
+    int numberOfWindows = 20; // Member Variable
+    int numberOfDoors = 2; // Member Variable
+    float height = 20.5f; // Member Variable
+    float width = 40.2f; // Member Variable
+    int numberOfFloors = 2; // Member Variable
+
+    void openWindows(bool itsTooHot); // Member Function
+    void closeWindows(bool itsTooHot); // Member Function
+    void turnOnHeating(bool itsTooCold, float temperature); // Member Function
+};
+
+
+struct Garden // UDT 2
+{
+    int numberOfPlants = 100; // Member Variable
+    bool hasPond = true; // Member Variable
+    
+    void waterPlants(bool hoseOn, float waterPressure); // Member Function
+    void feedFish(float howMuchFood, bool fishAreFull); // Member Function
+};
+
+
+struct Train // UDT 3
+{
+    struct Carriage // UDT 4 (Nested)
+    {
+        int length = 100; // Member Variable
+        int numberOfSeats = 50; // Member Variable
+    };
+    
+    
+    int numberOfCarriages = 12; // Member Variable
+    int numberOfPassengers = 127; // Member Variable
+    bool isFull = false; // Member Variable
+    
+    void goToNextStation(int currentStationNumber, int nextStationNumber, int howFast, float distance); // Member Function
+};
+
+
+struct Motorcycle // UDT 5
+{
+    bool isFast = true; // Member Variable
+    bool isCool = true; // Member Variable
+    bool riderEgoInflated = true; // Member Variable
+    
+    void doASweetSkid(float tireTreadLost = 7.2f, bool fellowMotoristsIrritated = true); // Member Function
+    void pullAWheely(float frontWheelElevation, bool spouseImpressed); // Member Function
+    void revEngineLoudly(int soundPressureLevel, bool childrenAreScared); // Member Function
+};
+
+
+struct Zoo // UDT 6
+{
+    int numberOfVisitors = 400; // Member Variable
+    int numberOfTigers = 37; // Member Variable
+    int ticketPrice = 40;
+    
+    void takePayment(int promotionalDiscount = 20, bool discountApplied = false); // Member Function
+    void releaseTigers(bool gateOpen, bool tigersAngry, int fear, int panic); // Member Function
+};
+
+
+struct Aeroplane // UDT 7
+{
+    int numberOfSeats = 400; // Member Variable
+    int numberOfEngines = 4; // Member Variable
+    bool pilotCanFly; // Member Variable
+    bool enginesWork; // Member Variable
+    
+    void takeOff(bool permissionGranted, int speed, bool dontCrash); // Member Function
+};
+
+
+struct Sausage // UDT 8
+{
+    float length = 14.2f; // Memeber Variable
+    float diameter = 3.2f; // Memeber Variable
+    int tubularity = 3; // Memeber Variable
+    bool isCooked = false; // Member Variable
+    
+    void cook(int howLong, float temperature); // Member Function
+};
+
+
+struct Pig // UDT 9
+{
+    struct PigsEar // UDT 10 (Nested)
+    {
+        bool earIsFloppy = true; // Memeber Variable
+    };
+    
+    int roundness = 999; // Member Variable
+    float width = 999.0f; // Member Variable
+    float snoutLength = 33.3f; // Member Variable
+    PigsEar leftEar; // Member Variable of User Defined Type
+    PigsEar rightEar; // Member Variable of User Defined Type
+    
+    
+    bool strokeOtherEar(PigsEar previousEar, PigsEar newEar ); // Member Function Using UDT Arguments
+    
+    void beFriendly(int  lovelinessFactor = 72, bool smellsTastyOats = true); // Member Function
+    void trot(int howFar, int howFast); // Member Function
+};
+
 int main()
 {
     std::cout << "good to go!" << std::endl;
+    return 0;    
 }
+
+
